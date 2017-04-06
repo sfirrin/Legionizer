b5e92c46c97fee2f6309659cc6fac487491ccb54
Stephen Firrincieli <sfirrin@unc.edu>
2017-04-06 18:23:50 -0400

    Finish creator form

--- legion-credits/src/CardCreator.js
+++ legion-credits/src/CardCreator.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import Textarea from 'react-textarea-autosize'
+
 import Background from './Background'
 import {getRandomDimensions} from './helpers'
 import './CardCreator.css'
@@ -19,31 +21,31 @@ class CardForm extends React.Component {
         const inputName = event.target.name
         if (inputName === 'names') {
             this.setState({names: event.target.value.toUpperCase()})
+            this.props.handleInputChange(this.props.index, inputName, event.target.value.toUpperCase())
         }
         if (inputName === 'title') {
             this.setState({title: event.target.value.toLowerCase()})
+            this.props.handleInputChange(this.props.index, inputName, event.target.value.toLowerCase())
         }
-        event.target.style.height = event.target.scrollHeight + 'px'
     }
 
     render() {
-        
+        const titlePlaceholder = this.props.placeholder.title || '...'
         return (
             <div className='input-row'>
-                <textarea 
+                <Textarea 
                     name='title' 
                     className='title-input'
-                    placeholder={this.props.placeholder.title}
+                    placeholder={titlePlaceholder}
                     value={this.state.title} 
                     onChange={this.handleChange}
                 />
-                <textarea
+                <Textarea
                     name='names' 
                     className='names-input'
-                    placeholder={this.props.placeholder.names.join('\n')}
+                    placeholder={this.props.placeholder.names.join('\n').toUpperCase()}
                     value={this.state.names} 
-                    onChange={this.handleChange} 
-                    style={{height: this.props.placeholder.names.length + 'em'}}
+                    onChange={this.handleChange}
                 />
             </div>
         );
@@ -55,21 +57,42 @@ class CardCreator extends Component {
         super(...args)
         this.state = {
             purple: getRandomDimensions(),
-            green: getRandomDimensions()
+            green: getRandomDimensions(),
+            cardValues: []
         }
+        // this.reportState = this.reportState.bind(this)
+        this.submit = this.submit.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+    submit() {
+        console.log(this.state)
+    }
+    handleInputChange(index, inputName, value) {
+        const newValues = this.state.cardValues.slice()
+        if (!newValues[index]) {
+            newValues[index] = {}
+        }
+        newValues[index][inputName] = value
+        this.setState({
+            cardValues: newValues
+        })
     }
     render() {
         let placeholderCards = []
-        for (let holder of defaultCredits) {
+        defaultCredits.map((holder, index) => {
             placeholderCards.push(
-                <CardForm placeholder={holder} />
+                <CardForm index={index} key={index} placeholder={holder} handleInputChange={this.handleInputChange}/>
             )
-        }
+        })
         return (
             <div>
                 <Background purple={this.state.purple} green={this.state.green} />
                 <div className='creator-container'>
+                    <div className='instructions'>
+                        Replace these titles and names with your own
+                    </div>
                     {placeholderCards}
+                    <button value="Submit" onClick={this.submit}>Button text</button>
                 </div>
             </div>
         )
