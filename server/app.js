f171fca5965c6c5684b0d32ca3794faf7448c462
Stephen Firrincieli <sfirrin@unc.edu>
2017-04-08 18:44:44 -0400

    Make another change to the index.html path

--- server/app.js
+++ server/app.js
@@ -58,7 +58,7 @@ app.post('/credits', (req, res) => {
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
 });
 
 module.exports = app;
